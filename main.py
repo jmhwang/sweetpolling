942901552a3cfb4fe26a7b9848b34f560b7ce2bd
kchwang <nyasss@gmail.com>
2018-08-08 21:51:34 +0900

    web app에 맞도록 style 적용 & style 테스트중

--- main.py
+++ main.py
@@ -6,7 +6,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
 	return render_template('51ox.html')
-    #return 'Hello World!'
+	#return 'Hello World!'
+
+@app.route('/example')
+def example():
+	return render_template('example.html')
 
 if __name__ == '__main__':
     app.debug = True
