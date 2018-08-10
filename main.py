138a753ed18cfea94873c64257a680802e640bb5
jmhwang <nyasss@gmail.com>
2018-08-10 18:11:07 +0900

    template 추가. css folder 위치 변경

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_world():
-	return render_template('51ox.html')
-	#return 'Hello World!'
+def R01name():
+    return render_template('01name.html')
+
+
+@app.route('/w', methods=['GET'])
+def R02waitting():
+    name = request.args.get('n','')
+    pid = request.args.get('p','')
+    return render_template('02waitting.html', name=name, pid=pid)
+
+
+@app.route('/p')
+def R03poll():
+    return render_template('03poll.html')
+
+
+@app.route('/s')
+def R04submit():
+    return render_template('04submit.html')
+
 
 @app.route('/example')
 def example():
-	return render_template('example.html')
+    return render_template('example.html')
+
 
 if __name__ == '__main__':
     app.debug = True
